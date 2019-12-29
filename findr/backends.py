caf2e02969de93c6d6176c2dd40809b78073fd62
Samson <samsonadejoro@gmail.com>
2019-12-29 22:28:33 +0100

    Fixed error message

--- findr/backends.py
+++ findr/backends.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.backends import ModelBackend
 from .models import User
 from django.http import Http404
+from rest_framework import response
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+
 
 class LoginBackend(ModelBackend):
     def authenticate(self, request, **kwargs):
@@ -8,5 +15,6 @@ class LoginBackend(ModelBackend):
         try:
             return User.objects.get(phone_number=phone_number)
         except User.DoesNotExist:
-            raise Http404
+            return response({'error': 'Invalid Credentials'},
+                        status=HTTP_404_NOT_FOUND)
            
